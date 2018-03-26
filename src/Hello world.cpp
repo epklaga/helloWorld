0a5cbbe0d72f8c8b74547363eb81a977f86d1f0d
EPKLAGA <EPKLAGA@SE00026772.ericsson.se>
2018-03-26 09:46:02 +0200

    first check in in eclipse

---
+++ src/Hello world.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "start"
+
+	return 0;
+}
